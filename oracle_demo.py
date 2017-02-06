c4b8d44ded5acd3205bcf00cd8c4c5d1293b5f0b
BaiJingjiao <baijingjiao1977@gmail.com>
2017-02-06 14:58:33 +0800

    Update oracle_demo.py

--- oracle_demo.py
+++ oracle_demo.py
@@ -6,11 +6,11 @@ instantclient-basic-win32-11.2.0.1.0.zip
 import cx_Oracle
 import pyodbc  
 
-oracle_IP = '10.174.3.1'
-oracle_PORT = '1521'
-oracle_ACCOUNT = 'ecs_0725'
-oracle_PWD = 'ecs_0725'
-SID = 'eSpace' #数据库实例ora11g
+oracle_IP = ''
+oracle_PORT = ''
+oracle_ACCOUNT = ''
+oracle_PWD = ''
+SID = '' #数据库实例ora11g
 
 #连接oracle数据库
 Oracle_connect = oracle_ACCOUNT + '/'+ oracle_PWD +'@'+ oracle_IP +':' + oracle_PORT + '/' + SID
